178a3313a87ebe67d69ffe184c596d57bd4e45e5
chilux2 <chilu.chilufya@hotmail.co.uk>
2023-02-18 14:26:28 +0000

    mixed messages vid

---
+++ script.js
@@ -0,0 +1,17 @@
+let view = document.getElementById('openM');
+let close = document.getElementById('closeM');
+let mixedM = document.getElementById('mixedM');
+
+let open = function(){
+    mixedM.style.display = 'block';
+    close.style.display = 'block';
+}
+
+let hide = function(){
+    mixedM.style.display = 'none';
+    close.style.display = 'none';
+}
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
+
