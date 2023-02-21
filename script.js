98e58bd4cb0872abd10ac33e8a8c36308bd60dab
chilux2 <chilu.chilufya@hotmail.co.uk>
2023-02-21 16:26:03 +0000

    new pic commit

--- script.js
+++ script.js
@@ -17,5 +17,41 @@ view.addEventListener('click', open);
 close.addEventListener('click', hide);
 
 
+//---------------------------------------------//
+let view1 = document.getElementById('openW');
+let close1 = document.getElementById('closeW');
+let weFixM = document.getElementById('weFixM');
+
+let openOne = function(){
+    weFixM.style.display = 'block';
+    close1.style.display = 'block';
+}
+
+let hideOne = function(){
+    weFixM.style.display = 'none';
+    close1.style.display = 'none';
+}
 
 
+view1.addEventListener('click',openOne);
+close1.addEventListener('click',hideOne);
+
+//--------------------------------//
+
+let view2 = document.getElementById('openK');
+let close2 = document.getElementById('closeK');
+let karateM = document.getElementById('karateM');
+
+let openThree = function(){
+    karateM.style.display = 'block';
+    close2.style.display = 'block';
+}
+
+let hideThree = function(){
+    karateM.style.display = 'none';
+    close2.style.display = 'none';
+}
+
+
+view2.addEventListener('click',openThree);
+close2.addEventListener('click',hideThree);
